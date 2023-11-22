9b18f37962aff60401f679636fe3eb90e5ae23e6
mariacarol1na <128617169+mariacarol1na@users.noreply.github.com>
2023-11-22 12:14:28 -0300

    ok

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 function tocaSomAplausos(){
-    document.querySelector ('# som_tecla_aplausos').play();
+    document.querySelector ('#som_tecla_aplausos').play();
 }
 
-document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
-unction tocaSomAplausos(){
-    document.querySelector ('# som_tecla_aplausos').play();
-}
-
-document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
+document.querySelector('.tecla_aplausos').onclick=tocaSomAplausos;
